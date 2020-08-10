ac2e76ac2ba70b08931bedc4dcc8bfda55a9e468
Pål Hamel <pal.hamel@gmail.com>
2020-08-10 14:26:43 +0200

    add build dir

---
+++ build/precache-manifest.4546661aef1a91f0e4f04b380c9943f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0388f98fdef43260397f4e2199d5a51f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5519dbbbf365698928e2",
+    "url": "/static/css/main.59489703.chunk.css"
+  },
+  {
+    "revision": "f5128dfbdb5fc2bb933a",
+    "url": "/static/js/2.6bf69f72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6bf69f72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5519dbbbf365698928e2",
+    "url": "/static/js/main.12c94e6e.chunk.js"
+  },
+  {
+    "revision": "8125cab94b5b52a4a0cf",
+    "url": "/static/js/runtime-main.ece796eb.js"
+  }
+]);
